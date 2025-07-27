e4d3ce013738ceafb0f48951cb8de2cdf9ddf8a1
GustawB <gustaw.blachowski@gmail.com>
2025-07-27 16:39:01 +0200

    Link block_two in dispatch.

--- src/block_two.rs
+++ src/block_two.rs
@@ -74,5 +74,17 @@ fn cp_a_r8(r8: u8, console: &mut Console) {
 }
 
 pub fn dispatch(instr: u8, console: &mut Console) -> () {
-    arithm_a_imm8::<ADD, CARRY>(instr, console);
+    let r8: u8 = instr & 0x07;
+    let op: u8 = (instr << 2) >> 5;
+    match op {
+        0 => arithm_a_imm8::<ADD, NO_CARRY>(r8, console),
+        1 => arithm_a_imm8::<ADD, CARRY>(r8, console),
+        2 => arithm_a_imm8::<SUB, NO_CARRY>(r8, console),
+        3 => arithm_a_imm8::<SUB, CARRY>(r8, console),
+        4 => logic_a_r8::<AND>(r8, console),
+        5 => logic_a_r8::<XOR>(r8, console),
+        6 => logic_a_r8::<OR>(r8, console),
+        7 => cp_a_r8(r8, console),
+        _ => panic!("Invalid opcode in block two"),
+    }
 }
