3afe4f26f7d63d6fee13037b2d391649c5f7dc63
Gustaw Blachowski <gustaw.blachowski@gmail.com>
2025-08-05 20:40:29 +0200

    Refactor arg_register macro

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,10 +1,10 @@
 extern crate proc_macro;
 use proc_macro::{TokenStream};
 use proc_macro2::{Span};
-use syn::{parse::{Parse, ParseStream}, parse_macro_input, Block, ExprParen, Ident, ItemFn, Path, Result, Token};
+use syn::{parse::{Parse, ParseStream}, parse_macro_input, parse_str, Block, Type, ExprParen, Ident, ItemFn, Path, Result, Token};
 use quote::{quote, ToTokens};
 
-struct MakeAnswerInput {
+struct MatchValueInput {
     ident: Ident,
     _comma: Token![,],
     pat: Path,
@@ -13,9 +13,9 @@ struct MakeAnswerInput {
     block: Block,
 }
 
-impl Parse for MakeAnswerInput {
+impl Parse for MatchValueInput {
     fn parse(input: ParseStream) -> Result<Self> {
-        Ok(MakeAnswerInput {
+        Ok(MatchValueInput {
            ident: input.parse()?,
             _comma: input.parse()?,
             pat: input.parse()?,
@@ -28,7 +28,7 @@ impl Parse for MakeAnswerInput {
 
 #[proc_macro]
 pub fn match_value(_item: TokenStream) -> TokenStream {
-    let MakeAnswerInput { ident, _comma, pat, paren_ident, block, .. } = parse_macro_input!(_item as MakeAnswerInput);
+    let MatchValueInput { ident, _comma, pat, paren_ident, block, .. } = parse_macro_input!(_item as MatchValueInput);
     quote! {
         match #ident {
             #pat #paren_ident => #block,
@@ -37,39 +37,67 @@ pub fn match_value(_item: TokenStream) -> TokenStream {
     }.into()
 }
 
-#[proc_macro_attribute]
-pub fn arg_register(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let attr_str: &str = &attr.to_string();
-    let mut item_fun: ItemFn = parse_macro_input!(item as ItemFn);
+fn new_arg_tokens(register: &str) -> (Ident, Type, Ident, Path, Path, ExprParen) {
+    let val_ident: Ident = Ident::new(&(register.to_owned() + "_val"), Span::call_site());
+    let val_type: Type;
+    let reg_ident: Ident = Ident::new(&(register.to_owned() + "_reg"), Span::call_site());
+    let regsize_path: Path;
+    let valuesize_path: Path;
+    let paren_ident: ExprParen;
 
-    match attr_str {
+    match register {
         "r8" => {
-            item_fun.block.stmts.insert(0, syn::parse(quote! { let r8_val: u8; }.into()).unwrap());
-            item_fun.block.stmts.insert(1, syn::parse(quote! { let r8_reg: &Value = &console.registers[RegSize::Byte(r8)]; }.into()).unwrap());
-            item_fun.block.stmts.insert(2, syn::parse(quote! { match_value!(r8_reg, Value::Byte(r) => { r8_val = **r; }); }.into()).unwrap());
+            val_type = parse_str("u8").expect("Failed to parse val type");
+            regsize_path = parse_str("RegSize::Byte").expect("Failed to parse regsize_path");
+            valuesize_path = parse_str("Value::Byte").expect("Failed to parse valuesize_path");
+            paren_ident = parse_str("(r8)").expect("Failed to parse paren_ident");
         },
         "r16" => {
-            item_fun.block.stmts.insert(0, syn::parse(quote! { let r16_val: u16; }.into()).unwrap());
-            item_fun.block.stmts.insert(1, syn::parse(quote! { let r16_reg: &Value = &console.registers[RegSize::Word(r16)]; }.into()).unwrap());
-            item_fun.block.stmts.insert(2, syn::parse(quote! { match_value!(r16_reg, Value::Word(r) => { r16_val = **r; }); }.into()).unwrap());
+            val_type = parse_str("u16").expect("Failed to parse val type");
+            regsize_path = parse_str("RegSize::Word").expect("Failed to parse regsize_path");
+            valuesize_path = parse_str("Value::Word").expect("Failed to parse valuesize_path");
+            paren_ident = parse_str("(r16)").expect("Failed to parse paren_ident");
         },
         "a" => {
-            item_fun.block.stmts.insert(0, syn::parse(quote! { let a_val: u8; }.into()).unwrap());
-            item_fun.block.stmts.insert(1, syn::parse(quote! { let a_reg: &Value = &console.registers[RegSize::Byte(A)]; }.into()).unwrap());
-            item_fun.block.stmts.insert(2, syn::parse(quote! { match_value!(a_reg, Value::Byte(r) => { a_val = **r; }); }.into()).unwrap());
+            val_type = parse_str("u8").expect("Failed to parse val type");
+            regsize_path = parse_str("RegSize::Byte").expect("Failed to parse regsize_path");
+            valuesize_path = parse_str("Value::Byte").expect("Failed to parse valuesize_path");
+            paren_ident = parse_str("(A)").expect("Failed to parse paren_ident");
         },
         "c" => {
-            item_fun.block.stmts.insert(0, syn::parse(quote! { let c_val: u8; }.into()).unwrap());
-            item_fun.block.stmts.insert(1, syn::parse(quote! { let c_reg: &Value = &console.registers[RegSize::Byte(C)]; }.into()).unwrap());
-            item_fun.block.stmts.insert(2, syn::parse(quote! { match_value!(c_reg, Value::Byte(r) => { c_val = **r; }); }.into()).unwrap());
+            val_type = parse_str("u8").expect("Failed to parse val type");
+            regsize_path = parse_str("RegSize::Byte").expect("Failed to parse regsize_path");
+            valuesize_path = parse_str("Value::Byte").expect("Failed to parse valuesize_path");
+            paren_ident = parse_str("(C)").expect("Failed to parse paren_ident");
         },
         "hl" => {
-            item_fun.block.stmts.insert(0, syn::parse(quote! { let hl_val: u16; }.into()).unwrap());
-            item_fun.block.stmts.insert(1, syn::parse(quote! { let hl_reg: &Value = &console.registers[RegSize::Word(HL)]; }.into()).unwrap());
-            item_fun.block.stmts.insert(2, syn::parse(quote! { match_value!(hl_reg, Value::Word(r) => { hl_val = **r; }); }.into()).unwrap());    
+            val_type = parse_str("u16").expect("Failed to parse val type");
+            regsize_path = parse_str("RegSize::Word").expect("Failed to parse regsize_path");
+            valuesize_path = parse_str("Value::Word").expect("Failed to parse valuesize_path");
+            paren_ident = parse_str("(HL)").expect("Failed to parse paren_ident");
         },
         _ => panic!("Invalid register parameter. Possible args are: r8; r16; a; c; hl"),
     };
+
+    (val_ident, val_type, reg_ident, regsize_path, valuesize_path, paren_ident)
+}
+
+#[proc_macro_attribute]
+pub fn arg_register(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let register: &str = &attr.to_string();
+    let mut item_fun: ItemFn = parse_macro_input!(item as ItemFn);
+
+    let (vi, vt, ri, rp, vp, pi) = new_arg_tokens(register);
+
+    item_fun.block.stmts.insert(0, syn::parse(quote! {
+        let #vi: #vt;
+    }.into()).expect("Failed to declare #val_ident"));
+    item_fun.block.stmts.insert(1, syn::parse(quote! {
+        let #ri: &Value = &console.registers[#rp #pi];
+    }.into()).expect("Failed to get #reg_ident"));
+    item_fun.block.stmts.insert(2, syn::parse(quote! {
+        match_value!(#ri, #vp (r) => { #vi = **r;});
+    }.into()).expect("Failed to set #val_ident"));
 
     item_fun.into_token_stream().into()
 }
